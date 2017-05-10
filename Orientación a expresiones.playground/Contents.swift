5d0c957ea4eea60c2a0b11b7fc94dedb0da06a78
Swift Funcional <info@swiftfuncional.com>
2017-05-10 13:17:30 +0200

    Split `check:dni` method into `validate:dni` and `characters:of`

--- Orientación a expresiones.playground/Contents.swift
+++ Orientación a expresiones.playground/Contents.swift
@@ -1,17 +1,19 @@
-func check(dni: String) -> Bool {
-    let letters: [Character] = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"]
-    
-    var dniArray = Array(dni.characters)
-    
-    if dniArray.count == 8 {
-        dniArray.insert("0", at: 0)
+func characters(of dni: String) -> [Character] {
+    if dni.characters.count == 8 {
+        return ["0"] + Array(dni.characters)
     }
     
-    if dniArray.count != 9 {
+    return Array(dni.characters)
+}
+
+func validate(_ dni: [Character]) -> Bool {
+    let letters: [Character] = ["T", "R", "W", "A", "G", "M", "Y", "F", "P", "D", "X", "B", "N", "J", "Z", "S", "Q", "V", "H", "L", "C", "K", "E"]
+    
+    if dni.count != 9 {
         return false
     }
     
-    let dniNumber = String(dniArray[0...7])
+    let dniNumber = String(dni[0...7])
     
     guard let number = Int(dniNumber) else {
         return false
@@ -21,11 +23,15 @@ func check(dni: String) -> Bool {
     
     let letter = letters[mod]
     
-    if dniArray[8] != letter {
+    if dni[8] != letter {
         return false
     }
     
     return true
+}
+
+func check(dni: String) -> Bool {
+    return validate(characters(of: dni))
 }
 
 check(dni: "7124223L")
